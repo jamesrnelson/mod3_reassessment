d1ee939232808a75a662572a3409624bc499fb7a
JNelson <nelson.jimmy@gmail.com>
2018-06-04 14:36:45 -0600

    Wrote test for posting to /api/v1/items

--- spec/requests/api/v1/items/items_delete_and_post_spec.rb
+++ spec/requests/api/v1/items/items_delete_and_post_spec.rb
@@ -38,6 +38,30 @@ end
 
 describe 'post request to /api/v1/items' do
   it 'should be able to post' do
-    
+    item1 = Item.new(
+      name: Faker::Commerce.product_name,
+      description: Faker::Lorem.paragraph,
+      image_url: "http://robohash.org/1.png?set=set2&bgset=bg1&size=200x200"
+    )
+
+    expected = [
+      {
+        'id': item1.id,
+        'name': item1.name,
+        'description': item1.description,
+        'image_url': item1.image_url
+      }
+    ]
+
+    post "/api/v1/items?name=#{item1.name}&description=#{item1.description}&image_url=#{item1.image_url}"
+    # When I send a POST request to `/api/v1/items` with a name, description, and image_url
+
+    expect(response).to be_success
+    # I receive a 201 JSON  response if the record is successfully created
+
+    new_item = JSON.parse(response.body, symbolize_names: true)
+
+    # And I receive a JSON response containing the id, name, description, and image_url but not the created_at or updated_at
+    expect(new_item).to eq(expected)
   end
 end
