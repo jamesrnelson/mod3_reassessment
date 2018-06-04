78675bf9ec72650a3ef37014de976111ab512c1b
JNelson <nelson.jimmy@gmail.com>
2018-06-04 12:21:42 -0600

    wrote the first feature test

---
+++ spec/features/user_can_search_for_cuisines_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'User' do
+  context 'enters Denver in the search' do
+    it 'should send back information about cuisines and restaurants' do
+      visit root_path
+      # As a user
+      # When I visit "/"
+      fill_in :search, with: 'Denver'
+      click_on 'Search'
+      # And I fill in a search box with "Denver" and click "search"
+      # Then my current path should be "/search" (ignoring params)
+      expect(page).to have_css('.cuisine', count: 5)
+      expect(page).to have_css('.restaurant', count: 5)
+      # And I should see the top 5 cuisines of Denver.
+      # And I should see the top 5 restaurants of Denver
+      expect(page).to have_css('.name', count: 5)
+      expect(page).to have_css('.address', count: 5)
+      expect(page).to have_css('.locality', count: 5)
+      # And I should see the name, address and locality, for each of the 5 restaurants
+      # *BOSS MODE* when I click page 2,
+      # I should see the remaining 5 restaurants.
+    end
+  end
+end
