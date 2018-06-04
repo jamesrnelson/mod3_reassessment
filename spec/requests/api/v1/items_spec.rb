8cd22d86e43500b9aadf870ebd394db1d3624fb3
JNelson <nelson.jimmy@gmail.com>
2018-06-04 14:15:05 -0600

    Added test for api v1 items show. Added route

--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -41,3 +41,28 @@ describe 'get request to api/v1/items' do
     expect(items).to eq(expected)
   end
 end
+
+describe 'get request /api/v1/items/:id' do
+  it 'should respond with info about that specific item' do
+    item1 = Item.create!(
+      name: Faker::Commerce.product_name,
+      description: Faker::Lorem.paragraph,
+      image_url: "http://robohash.org/1.png?set=set2&bgset=bg1&size=200x200"
+    )
+
+    get "/api/v1/items/#{item1.id}"
+
+    # When I send a GET request to `/api/v1/items/1`
+    expected = {
+        'id': item1.id,
+        'name': item1.name,
+        'description': item1.description,
+        'image_url': item1.image_url
+      }
+    expect(response).to be_success
+
+    item = JSON.parse(response.body, symbolize_names: true)
+    # I receive a 200 JSON response containing the id, name, description, and image_url but not the created_at or updated_at
+    expect(item).to eq(expected)
+  end
+end
